77cdeab43a9caebcc45341d60e47357e8c59011e
ParkJuhyeong <majecty@gmail.com>
2020-12-02 15:16:21 +0900

    기호 레이어에 " 추가

--- keyboards/gboards/k/gergoplex/keymaps/jh/keymap.c
+++ keyboards/gboards/k/gergoplex/keymaps/jh/keymap.c
@@ -52,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 /* Keymap 1: Symbols layer
  * ,-----------------------------.       ,--------------------------------.
- * |  !   |  @  |  {  |  }  |  |  |      |  `  |  ~  |     |     |    \   |
+ * |  !   |  @  |  {  |  }  |  |  |      |  `  |  ~  |  "  |     |    \   |
  * |-----+-----+-----+-----+------|      |--------------------------------|
  * |  #   |  $  |  (  |  )  | LMB |      |  +  |  -  |  /  |  *  |    '   |
  * |-----+-----+-----+-----+------+      |--------------------------------|
@@ -63,7 +63,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *  				'-----------------'           '------------------'
  */
 [SYMB] = LAYOUT_gergoplex(
-    KC_EXLM, KC_AT,   KC_LCBR, KC_RCBR, KC_PIPE,   KC_GRV,  KC_TILD, KC_TRNS, KC_TRNS, KC_BSLS,
+    KC_EXLM, KC_AT,   KC_LCBR, KC_RCBR, KC_PIPE,   KC_GRV,  KC_TILD, KC_DQUO, KC_TRNS, KC_BSLS,
     KC_HASH, KC_DLR,  KC_LPRN, KC_RPRN, KC_BTN2,   KC_PLUS, KC_MINS, KC_SLSH, KC_ASTR, KC_QUOT,
     KC_PERC, KC_CIRC, KC_LBRC, KC_RBRC, KC_BTN1,   KC_AMPR, KC_EQL,  KC_COMM, KC_DOT,  KC_MINS,
                       CMB_TOG, KC_SCLN, KC_EQL,   KC_EQL,  KC_SCLN, KC_DEL
